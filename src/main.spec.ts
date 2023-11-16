2ae6ab1c248de5521c74854a41bbee0bdf6361ac
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-11-16 18:59:48 +0000

    :rotating_light: should be leap if is not divisible by 100

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -3,8 +3,18 @@ import { describe, it, expect } from "vitest"
 import { isYearLeap } from "./main.js"
 
 describe("Default test", () => {
-  it("should return true if is divisible by 4", () => {
-    const result = isYearLeap(new Date())
+  it("shouldn't be leap if isn't divisible by 4", () => {
+    const actualDate = new Date(Date.now())
+
+    const result = isYearLeap(actualDate)
+
+    expect(result).toBe(false)
+  })
+
+  it("should be leap if is not divisible by 100", () => {
+    const actualDate = new Date(Date.now())
+
+    const result = isYearLeap(actualDate)
 
     expect(result).toBe(true)
   })
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,3 @@
 export function isYearLeap(year: Date) {
-  return true
+  return false
 }
