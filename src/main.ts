0ec0430392fe09dcdd53903a0183d4ab4e7abc8e
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-11-16 19:24:39 +0000

    :hammer: refactor extracted conditional methods

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
+const isDivisibleByFour = (year: number) => year % 4 === 0
+const isntDivisibleByOneHundred = (year: number) => year % 100 !== 0
+
 export function isYearLeap(date: Date) {
   const year = date.getFullYear()
 
-  if (year % 4 === 0 && year % 100 !== 0) return true
+  if (isDivisibleByFour(year) && isntDivisibleByOneHundred(year)) return true
 
   return false
 }
